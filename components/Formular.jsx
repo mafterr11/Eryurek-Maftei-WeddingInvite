3140a99687787beb30984800495227387475eb0e
mafterr11 <mafterr11@gmail.com>
2025-03-13 21:31:27 +0200

    .

--- components/Formular.jsx
+++ components/Formular.jsx
@@ -8,23 +8,21 @@ import { useState } from "react";
 
 // Full validation schema
 const fullSchema = z.object({
-  nume: z.string().min(2, { message: "Va rog adaugati-va numele" }),
+  nume: z.string().min(2, { message: "Va rog adaugati-va numele!" }),
+  prenume: z.string().min(2, { message: "Va rog adaugati-va prenumele!" }),
   numarPersoane: z.enum(["o persoana", "doua persoane"], {
-    message: "Selectati cate persoane",
+    message: "Selectati cate persoane!",
   }),
   copii: z.enum(["nu", "1 copil", "2 copii", "3 copii", "4 copii", "5 copii"], {
     message: "Cati copii vor veni?",
-  }),
-  cazare: z.enum(["da", "nu"], { message: "Este nevoie de cazare?" }),
-  meniu: z.enum(["meniu standard", "meniu vegetarian"], {
-    message: "Ce tip de meniu doriti?",
   }),
   attending: z.enum(["yes", "no"], { message: "Va rog completati" }),
 });
 
 // Decline validation schema (only name required)
 const declineSchema = z.object({
-  nume: z.string().min(2, { message: "Va rog adaugati-va numele" }),
+  nume: z.string().min(2, { message: "Va rog adaugati-va numele!" }),
+  prenume: z.string().min(2, { message: "Va rog adaugati-va prenumele!" }),
   attending: z.enum(["yes", "no"]),
 });
 
@@ -47,7 +45,7 @@ const Formular = () => {
   };
 
   return (
-    <div className="relative flex h-full items-center justify-center ">
+    <div className="relative flex h-full items-center justify-center">
       <form
         onSubmit={validateForm(submitForm)}
         className="border-accentGreen flex flex-col items-center gap-8 rounded-xs border-2 bg-[#f8f8f7] p-5"
@@ -70,6 +68,20 @@ const Formular = () => {
           {errors.nume && (
             <p className="absolute top-full left-0 text-xs text-red-500">
               {errors.nume.message}
+            </p>
+          )}
+        </div>
+        {/* Surname Input */}
+        <div className="relative w-full">
+          <input
+            {...register("prenume")}
+            type="text"
+            placeholder="Prenumele tău"
+            className="w-full cursor-pointer border border-black p-2 text-black"
+          />
+          {errors.prenume && (
+            <p className="absolute top-full left-0 text-xs text-red-500">
+              {errors.prenume.message}
             </p>
           )}
         </div>
@@ -108,38 +120,6 @@ const Formular = () => {
           {errors.copii && (
             <p className="absolute top-full left-0 text-xs text-red-500">
               {errors.copii.message}
-            </p>
-          )}
-        </div>
-
-        <div className="relative w-full">
-          <select
-            {...register("cazare")}
-            className="w-full cursor-pointer border border-black p-2 text-black/50"
-          >
-            <option value="">Dorești cazare?</option>
-            <option value="da">Da</option>
-            <option value="nu">Nu</option>
-          </select>
-          {errors.cazare && (
-            <p className="absolute top-full left-0 text-xs text-red-500">
-              {errors.cazare.message}
-            </p>
-          )}
-        </div>
-
-        <div className="relative w-full">
-          <select
-            {...register("meniu")}
-            className="w-full cursor-pointer border border-black p-2 text-black/50"
-          >
-            <option value="">Alege tipul meniului</option>
-            <option value="meniu standard">Meniu standard</option>
-            <option value="meniu vegetarian">Meniu vegetarian</option>
-          </select>
-          {errors.meniu && (
-            <p className="absolute top-full left-0 text-xs text-red-500">
-              {errors.meniu.message}
             </p>
           )}
         </div>
