c17383a9a440dac385ff7412f9c67eff992cedf6
mafterr11 <mafterr11@gmail.com>
2026-02-08 21:24:14 +0200

    .

--- components/Alaturi.jsx
+++ components/Alaturi.jsx
@@ -2,7 +2,7 @@ import { Heart } from "lucide-react";
 
 export default function Alaturi() {
   return (
-    <section className="bg-alaturi relative flex h-[30rem] w-full items-center justify-center bg-cover max-md:bg-center bg-no-repeat bg-fixed bg-center md:h-[30rem]">
+    <section className="bg-alaturi relative flex h-[30rem] w-full items-center justify-center bg-cover max-md:bg-center bg-no-repeat bg-center bg-center md:h-[30rem]">
       <div className="absolute inset-0  bg-black/30" />
       <div className="relative text-center text-white">
         <div className="mb-2 flex justify-center">
